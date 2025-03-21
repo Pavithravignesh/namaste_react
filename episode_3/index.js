1d73a2c82c99e51cd3cb939a7f16a474534a9e1d
Pavithravignesh <pavithravignesh027@gmail.com>
2025-03-21 19:19:33 +0530

    episode_3 has been completed

--- episode_3/index.js
+++ episode_3/index.js
@@ -1,16 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// react element - object
-const heading = React.createElement("div", { id: "heading" }, "hello world using createElement and createRoot!"); // type object
-const root = ReactDOM.createRoot(document.getElementById("root")); // type object
+// jsx - is not html, it's just html like syntax even though it's like xml
+// React.createElement => ReactElement - JS Object => rendering using html element HTMLElement(render);
+const JSXHeading = (
+  <div>
+    <h5>Hello, world! using JSX</h5>
+    <h5>Hello, world! using JSX</h5>
+  </div>
+);
+const rootJSX = ReactDOM.createRoot(document.getElementById("root-jsx"));
 
-console.log(heading);
-console.log(root);
+// console.log(JSXHeading);
+// console.log(rootJSX);
+
+rootJSX.render(JSXHeading);
+
+// react component - a function which return a jsx;
+const HeadingComponentOne = () => {
+  return (
+    <>
+      <h1>HeadingComponentOne</h1>
+      <h3> description </h3>
+    </>
+  );
+};
+
+const HeadingComponentTwo = () => {
+  return (
+    <>
+      <HeadingComponentOne />
+      <h4>here: {reactElement}</h4>
+      <h1>HeadingComponentTwo</h1>
+      <h3> description </h3>
+    </>
+  );
+};
+
+const rootReactComponent = ReactDOM.createRoot(
+  document.getElementById("root-reactComponent")
+);
+rootReactComponent.render(<HeadingComponentTwo />);
+
+// React.createElement => ReactElement - JS Object => rendering using html element HTMLElement(render);
+const reactElement = React.createElement(
+  "div",
+  { id: "heading" },
+  "hello world using createElement and createRoot!"
+); // type object
+const rootCreateElement = ReactDOM.createRoot(
+  document.getElementById("root-createElement")
+); // type object
+
+// console.log(reactElement);
+// console.log(rootCreateElement);
 
 // render - does the conversion of js object into react elements
-root.render(heading);
-
-// jsx - is not html, it's just html like syntax even though it's like xml
-const JSXHeading = <h1>Hello, world! using JSX</h1>;
-
+rootCreateElement.render(reactElement);
--- episode_3/revise.md
+++ episode_3/revise.md
@@ -1,5 +1,43 @@
 1. what is react elements?
+
 - as like DOM Element in the browser
 
 2. what is jsx?
-js syntax to create react elements easily and effectively
+   js syntax to create react elements easily and effectively
+
+3. js engine what?
+
+- ECMAScript || es
+
+4. since, jsx is can't be handled by js, in this project parcel does the magic. the more correct way "Parcel is transpiled it before it reaches the borwser, using babel!
+
+5. what is babel?
+
+- js compiler
+
+6. what is should the case for attribute in jsx?
+
+- camel case!
+
+extension for better life;
+
+1. prettier
+2. bracket pair colorization toggler,
+3. ESLint,
+4. gitlens,
+5. better comments,
+
+6. what is react component?
+
+- class based components,
+- functional components,
+
+8. why can't I render a react component the same way I did for react react element?
+
+- babel can be only identify react components using </>; without this it'll might be end up considering it as a react element
+
+9. what is the difference between react element and components?
+
+10. what is component composition?
+
+11. what is cross site scripting?
